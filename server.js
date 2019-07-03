54ce3a6c52844c2eec04d0465d89e84f36b75297
Muhwezi Armstrong <armstrongsouljah@gmail.com>
2019-07-03 21:58:07 +0300

    chore(routing): Set up express routing
    - Create routes module
    - create initial routes
    - Register routes into the main app
    
    [Maintains #167075899]

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express  = require('express')
 const bodyParser = require('body-parser')
+const todoListRoutes = require('./routes/todoList')
 const port = 8000
 const app = express()
 
 // add body parser middleware
 app.use(bodyParser({ extended: true }))
+app.use(todoListRoutes)
+
+app.listen(port)
+console.log(`app running at http://localhost:${port}`)
